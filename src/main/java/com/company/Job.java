eb00ff95b990da51e8b422b8a71764a3c5101694
pdmxdd <pdmxdd@gmail.com>
2021-07-06 22:43:56 -0500

    part 3 Job.java constructors

--- src/main/java/com/company/Job.java
+++ src/main/java/com/company/Job.java
@@ -10,8 +10,20 @@ public class Job {
     private PositionType positionType;
     private CoreCompetency coreCompetency;
 
-    // TODO: add two constructors - one to initialize a unique ID and a second to initialize the other five fields. The second constructor should also call the first in order to initialize the 'id' field
+    // DONE: add two constructors - one to initialize a unique ID and a second to initialize the other five fields. The second constructor should also call the first in order to initialize the 'id' field
+    public Job() {
+        this.id = nextId;
+        nextId++;
+    }
 
+    public Job(String name, Employer employer, Location location, PositionType positionType, CoreCompetency coreCompetency) {
+        this();
+        this.name = name;
+        this.employer = employer;
+        this.location = location;
+        this.positionType = positionType;
+        this.coreCompetency = coreCompetency;
+    }
     // TODO: Add custom equals and hashCode methods. Consider two Job objects "equal" when their id fields match.
 
     // TODO: Add getters for each field EXCEPT nextId. Add setters for each field EXCEPT nextID and id.
