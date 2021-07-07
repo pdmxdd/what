4122de5b5743ea2db3b70313a39d14f5ca5640c3
pdmxdd <pdmxdd@gmail.com>
2021-07-06 22:48:45 -0500

    part 4 empty constructor test

--- src/test/java/com/company/incomplete/JobTests.java
+++ src/test/java/com/company/incomplete/JobTests.java
@@ -1,4 +1,17 @@
 package com.company.incomplete;
 
+import com.company.Job;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class JobTests {
+    @Test
+    public void testSettingJobId() {
+        Job testJobOne = new Job();
+        Job testJobTwo = new Job();
+        Assertions.assertEquals(1, testJobOne.getId());
+        Assertions.assertEquals(2, testJobTwo.getId());
+        Assertions.assertFalse(testJobOne.getId() == testJobTwo.getId());
+        Assertions.assertTrue(testJobTwo.getId() - testJobOne.getId() == 1);
+    }
 }
