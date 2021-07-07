7b2d3176d2cb741be713a6e7e1046daea004c38a
pdmxdd <pdmxdd@gmail.com>
2021-07-06 22:55:41 -0500

    part 4 test full constructor using instanceof

--- src/test/java/com/company/incomplete/JobTests.java
+++ src/test/java/com/company/incomplete/JobTests.java
@@ -1,6 +1,6 @@
 package com.company.incomplete;
 
-import com.company.Job;
+import com.company.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -9,9 +9,21 @@ public class JobTests {
     public void testSettingJobId() {
         Job testJobOne = new Job();
         Job testJobTwo = new Job();
-        Assertions.assertEquals(1, testJobOne.getId());
-        Assertions.assertEquals(2, testJobTwo.getId());
         Assertions.assertFalse(testJobOne.getId() == testJobTwo.getId());
         Assertions.assertTrue(testJobTwo.getId() - testJobOne.getId() == 1);
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields() {
+        Job testJob = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        Assertions.assertEquals("Product tester", testJob.getName());
+        Assertions.assertTrue(testJob.getEmployer() instanceof Employer);
+        Assertions.assertEquals("ACME", testJob.getEmployer().getValue());
+        Assertions.assertTrue(testJob.getLocation() instanceof Location);
+        Assertions.assertEquals("Desert", testJob.getLocation().getValue());
+        Assertions.assertTrue(testJob.getPositionType() instanceof PositionType);
+        Assertions.assertEquals("Quality control", testJob.getPositionType().getValue());
+        Assertions.assertTrue(testJob.getCoreCompetency() instanceof CoreCompetency);
+        Assertions.assertEquals("Persistence", testJob.getCoreCompetency().getValue());
     }
 }
